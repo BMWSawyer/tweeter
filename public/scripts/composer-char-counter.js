946e0dbd9eecbbf482db4a7a639489166a6b0084
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-09-15 18:13:00 +0000

    Refactor code after review

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  $("#tweet-text").parent("form").children(".form-submission").children(".counter").val(140)
+  let counter = 140;
+  
+  $("#tweet-text").parent("form").children(".form-submission").children(".counter").val(counter)
 
   $("#tweet-text").on('input', function() {
     const text = $(this).val();
-    let counter = 140;
-
+    
     counter -= text.length;
 
     let charCounter = $(this).parent("form").children(".form-submission").children(".counter").val(counter);
 
-    if (counter <= 0) {
+    if (counter < 0) {
       $(this).parent("form").children(".form-submission").children(".counter").addClass("non-valid");
-    } else if (counter > 0) {
+    } else {
       $(this).parent("form").children(".form-submission").children(".counter").removeClass("non-valid");
     }
 
