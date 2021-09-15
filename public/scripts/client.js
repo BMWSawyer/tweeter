ab3880f6e65b52f77020872b8949f62fbbc65580
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-09-15 18:12:36 +0000

    Implement timeago functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  let dateTime = timeago.format(1631465538527);
 
+  $(".date-time").text(dateTime);
+
+});
