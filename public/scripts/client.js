8ec26c7aa68d1fa1d0dfbbcb44098be0b753ee16
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-09-18 22:37:52 +0000

    Define function loadTweets. Implement logic to fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 $(document).ready(function() {
   
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   const renderTweets = function(tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
@@ -44,6 +19,7 @@ $(document).ready(function() {
   const createTweetElement = function(tweet) {
     
     let dateTime = timeago.format(tweet['created_at']);
+    
     
     const $tweet = $(`
       <article>
@@ -71,7 +47,6 @@ $(document).ready(function() {
     return $tweet;
   };
   
-  renderTweets(data);
 
   $('.form').on( 'submit', function( event ) {
     event.preventDefault();
@@ -86,18 +61,19 @@ $(document).ready(function() {
         console.log(error);
       })
   });
-  
-/*
-  const fetchPosts = $.ajax({
-    url: "",
+
+  const loadTweets = $.ajax({
+    url: "/tweets",
     method: "GET",
     dataType: "json",
     success: (tweets) => {
-      console.log(tweets)
+      renderTweets(tweets);
     },
     error: (error) => {
       console.log(error)
     }
   });
-*/
+
+  loadTweets();
+
 });
